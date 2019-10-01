9bbf2405f760ee9716f3bd4f7d76781faa548c87
kyuwonk <freddy.kim8@gmail.com>
2019-10-01 00:46:08 -0700

    Change to skip javascript

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Fa19Lab1
+module Fa19Lab3
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
